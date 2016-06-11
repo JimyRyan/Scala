dd25e5227447b924c1e94d21869d6e1ac79d56cc
JimyRyan <JimyRyan@gmail.com>
2016-06-11 13:35:48 +0200

    Create String.scala

---
+++ Sandbox/String.scala
@@ -0,0 +1,16 @@
+# Formating
+1. printf
+
+Only display the result
+
+```
+printf("%d.%d.%d %d:%d:%d", year, month, day, hour, minute, second)
+```
+
+2. format
+
+Output and/or store the result into a variable
+
+```
+"%d.%d.%d %d:%d:%d".format(year, month, day, hour, minute, second)
+```
